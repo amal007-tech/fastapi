423d9c968af763ef936dcd03fd6c549b8bad754d
amal007-tech <amalbiso01@gmail.com>
2024-12-11 09:53:54 +0530

    app/*

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from prediction import get_city_map, predict_price
 app=FastAPI()
 
-@app.get("/")
+@app.get("/app/predict")
 
-def index():
-  return {"message":"Hi Welcome to my world"}
+def predict(size,total_sqft,bath,balcony,location):
+  price =list(predict_price(size,total_sqft,bath,balcony,location))[0]
+  return {"message":"Success","data":{"predicted_price":price},"Errorcode":0}
+
+@app.get("/api/citylist")
+def get_city_map_api():
+    city_map = get_city_map()
+    return {"message":"Success","data":city_map,"Errorcode":0}
+    
